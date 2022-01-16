733825658fc03f64d5d46dc48f0cc1ed3eb62897
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2022-01-16 13:28:21 +0100

    Add files via upload

--- airport.php
+++ airport.php
@@ -10,9 +10,19 @@
 
   </head>
   <body>
-  <div id="ban1"></div>
-  <div id="ban2"></div>
-  <div id="glowny"></div>
+  <div id="ban1"><h2>Odloty z lotniska</h2></div>
+  <div id="ban2"><img src="zad6.png" alt="logotyp"></div>
+  <div id="glowny"><h4>tabela odlotów</h4>
+  <table>
+      <tr>
+          <th>lp</th> <th>numer rejsu</th><th>czas</th><th>kierunek</th><th>status</th>
+      </tr>
+      
+      
+      
+      
+  </table>
+  </div>
   <div id="stop1"></div>
   <div id="stop2"></div>
   <div id="stop3"></div>
